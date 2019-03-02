558984d4df536f73de39099a40fc1afb420fce8c
KrisOP <kortega070@gmail.com>
2019-03-01 20:05:36 -0600

    completado pero no verificado

--- chapter2/quiz1/questions/question_1.rb
+++ chapter2/quiz1/questions/question_1.rb
@@ -1,2 +1,13 @@
 # Write a program that uses constants to define the numbers 1 to 10 as
 # numerals.
+
+NUMERO1=1
+NUMERO2=2
+NUMERO3=3
+NUMERO4=4
+NUMERO5=5
+NUMERO6=6
+NUMERO7=7
+NUMERO8=8
+NUMERO9=9
+NUMERO10=10
