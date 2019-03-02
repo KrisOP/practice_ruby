a29d303560f8850db6dad0c58241cf1ead6c68bf
KrisOP <kortega070@gmail.com>
2019-03-01 21:16:49 -0600

    completado pero sin verificar

--- chapter4/quiz1/questions/question_3.rb
+++ chapter4/quiz1/questions/question_3.rb
@@ -6,3 +6,18 @@
 #     previous two Fibonacci numbers together e.g. the third Fibonacci
 #     number is the result of `0 + 1`.
 # * `puts`es these numbers, one per line.
+
+cont=0
+fibo1=0
+fibo2=1
+puts fibo1
+puts fibo2
+
+while cont <= 18
+    fibo3= fibo1 + fibo2
+    puts fibo3
+    fibo1=fibo2
+    fibo2=fibo3
+    cont+=1
+end
+    
