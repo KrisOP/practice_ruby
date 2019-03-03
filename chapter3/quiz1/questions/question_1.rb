081027528485657cdd13da31beb6646ee4897ef4
KrisOP <kortega070@gmail.com>
2019-03-02 18:12:11 -0600

    completado pero no verificado

--- chapter3/quiz1/questions/question_1.rb
+++ chapter3/quiz1/questions/question_1.rb
@@ -1,4 +1,8 @@
 # Uncomment each line and fill in the blanks so that the arithmetic
 # operations produce the correct value for each variable.  e.g. in
 # `four = 2.+()`, the calculation to the right of the `=` should
-# evaluate to 4. So it would become `four = 2.+(2)`.
+# evaluate to 4. So it would become `
+
+four = 2.+(2)
+
+puts four
